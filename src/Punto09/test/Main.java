3a081870cbf2d62ef0f2d725dbbe4a1948fb0087
grove <grove@DESKTOP-0L7LIBL>
2018-04-11 21:41:33 -0300

    Agregados Paquetes. Usuario: Rojas, Guido

---
+++ src/Punto09/test/Main.java
@@ -0,0 +1,38 @@
+package Punto09.test;
+
+import Punto09.modelo.Libro;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Rojas, Guido G.
+ */
+public class Main {
+    public static void main (String [] args)   {    
+        ArrayList<Libro> listaLibros = new ArrayList<Libro>();
+        //Cargando Lista de Libros
+        Libro objLibro = new Libro("Titulo uno","Autor Uno");
+        listaLibros.add(objLibro);
+        objLibro = new Libro("Titulo dos","Autor Dos");
+        listaLibros.add(objLibro);
+        objLibro = new Libro("Titulo tres","Autor Tres");
+        listaLibros.add(objLibro);
+        objLibro = new Libro("Titulo dos","Autor Tres");
+        listaLibros.add(objLibro);
+                
+        Scanner sca = new Scanner(System.in);
+        System.out.print("Ingrese titulo que quiere buscar: ");
+        String buscado = sca.nextLine();
+        
+        Iterator<Libro> iterador = listaLibros.iterator();
+        while(iterador.hasNext()) {
+            objLibro = iterador.next();
+            if(objLibro.getTitulo().compareToIgnoreCase(buscado) == 0) {
+                System.out.println("Libro encontrado");
+                System.out.println("Autor del Libro: "+objLibro.getAutor());
+            }
+        }        
+    }
+}
